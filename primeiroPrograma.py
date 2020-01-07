44a8fc7bae12e7cc7fc83858acd7950b33053bf7
theyCallMeDudu <52638570+theyCallMeDudu@users.noreply.github.com>
2020-01-07 20:34:46 -0300

    Add files via upload

---
+++ primeiroPrograma.py
@@ -0,0 +1,3 @@
+print('Meu primeiro programa em Python')
+print(1 + 2 + 9 + 10)
+print('Fim do programa')
---
+++ segundoPrograma.py
@@ -0,0 +1,3 @@
+print('Soma de 2 e 3:')
+print('O resultado de 2 + 3 é: ', 2 + 3)
+print('Fim do programa.')
